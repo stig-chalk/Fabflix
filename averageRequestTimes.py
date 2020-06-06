8abb830c8d9cbe98a7bf848ad4ac74bfabb46034
Yuhui Chen <fenbihuichen@gmail.com>
2020-06-05 20:04:41 -0700

    update py

--- averageRequestTimes.py
+++ averageRequestTimes.py
@@ -1,15 +1,20 @@
 try:
-	file = open(input("path 2 requestTimes.txt: "), "r")
+	file = open(input("path 2 requestTimes.txt: ").rstrip(), "r")
 	lines = file.readlines()
-	ts = tj = requests = 0
+	ts = tj = tscount = tjcount = 0
 	for line in lines:
 		line.rstrip("\n")
-		requests += 1
 		temp = line.split(",")
-		tj += int(temp[0])
-		ts += int(temp[1])
+		if len(temp) > 0:
+			tj += int(temp[0])
+			tjcount += 1
+		if len(temp) > 1:
+			ts += int(temp[1])
+			tscount += 1
+		
+
 	file.close()
-	print("Average TS: " + str(ts / requests))
-	print("Average TJ: " + str(tj / requests))
+	print("Average TS: " + str(ts / tscount))
+	print("Average TJ: " + str(tj / tjcount))
 except IOError:
 	print("Invalid path")
